65f04f9aed48fc6d41dd82a3a5e0300389e006cf
Skualid <jl.gallego.2019@alumnos.urjc.es>
2022-03-08 18:13:57 +0100

    Ejercicio 2 y 3 corregidos

--- src/Ejercicio2.py
+++ src/Ejercicio2.py
@@ -39,7 +39,7 @@ def sql_insert_json_user(con):
 
             telefono = data['usuarios'][usuario][nombre]['telefono']
             if telefono == 'None':
-                telefono = 0
+                telefono = -1
 
             contraseña = data['usuarios'][usuario][nombre]['contrasena']
             provincia = data['usuarios'][usuario][nombre]['provincia']
@@ -126,23 +126,64 @@ def apartado_1(con):
    #Contar número de muestras
    cursorObj.execute('SELECT distinct * FROM usuarios')
    user = cursorObj.fetchall()
-
    pd1 = pd.DataFrame(user)
-   print("El númeor de muestras de usuarios es: ", pd1[1].count())
+   pd1.rename(columns={0:"nombre", 1:"telefono", 2:"contraseña", 3:"provincia", 4:"permisos", 5:"email_total", 6:"email_phishing", 7:"email_clicados"}, inplace=True)
 
    cursorObj.execute('SELECT distinct * FROM legal')
    legal = cursorObj.fetchall()
-
    pd2 = pd.DataFrame(legal)
-   print("El númeor de muestras de webs es: ", pd2[1].count())
+   pd2.rename(columns={0:"url", 1:"cookies", 2:"aviso", 3:"proteccion_de_datos", 4:"creacion"}, inplace=True)
+
+   cursorObj.execute('SELECT distinct * FROM fecha')
+   fecha = cursorObj.fetchall()
+   pd3 = pd.DataFrame(fecha)
+   pd3.rename(columns={0:"nombre", 1:"fecha", 2:"numero_de_fechas_repetidas"}, inplace=True)
+
+   cursorObj.execute('SELECT distinct * FROM ip')
+   ip = cursorObj.fetchall()
+   pd4 = pd.DataFrame(ip)
+   pd4.rename(columns={0:"nombre", 1:"ip"}, inplace=True)
+
+   print("APARTADO 1\n-------------------------------------------\n" + "Número de muestras\n")
+   print("El número de muestras de usuarios es: ", pd1['nombre'].count())
+   print("El número de muestras de webs es: ", pd2['url'].count())
 
    #Contar valores missing
-   print("El número de valores missing en la columna telefono es: ", pd1[1].value_counts().loc[0])
-   print("El número de valores missing en la columna provincia es: ", pd1[3].value_counts()["None"])
-   cursorObj.execute('SELECT distinct * FROM ip')
-   ips = cursorObj.fetchall()
-   pd3 = pd.DataFrame(ips)
-   print("El número de valores missing de ips es: ", pd3[1].value_counts().loc["None"])
+   print("-------------------------------------------\n" + "Valores missing en tabla usuarios\n")
+   for columna in pd1:
+       contador = 0
+       for fila in pd1[columna]:
+           if str(fila) == 'None' or str(fila) == '-1' or str(fila) == '':
+               contador += 1
+       print("En la columna",columna , "hay", contador, "missing")
+
+   print("-------------------------------------------\n" + "Valores missing en tabla fecha\n")
+   for columna in pd3:
+       contador = 0
+       for fila in pd3[columna]:
+           if str(fila) == 'None' or str(fila) == '-1' or str(fila) == '':
+               contador += 1
+       print("En la columna", columna, "hay", contador, "missing")
+
+   print("-------------------------------------------\n" + "Valores missing en tabla ip\n")
+   for columna in pd4:
+       contador = 0
+       for fila in pd4[columna]:
+           if str(fila) == 'None' or str(fila) == '-1' or str(fila) == '':
+               contador += 1
+       print("En la columna", columna, "hay", contador, "missing")
+
+
+   print("-------------------------------------------\n" + "Valores missing en tabla legal\n")
+   for columna in pd2:
+       contador = 0
+       for fila in pd2[columna]:
+           if str(fila) == 'None' or str(fila) == '-1' or str(fila) == '':
+               contador += 1
+
+       print("En la columna", columna, "hay", contador, "missing")
+
+   print("-------------------------------------------\n\nAPARTADO 2")
 
 def apartado_2(con):
    cursorObj = con.cursor()
@@ -152,9 +193,9 @@ def apartado_2(con):
    pd1 = pd.DataFrame(rows)
 
    pd1[1] += pd1[2] #sumo al total de fechas de cada usuario las repetidas
-   print(pd1)
    print("El valor de la media es: ", pd1[1].mean())
    print("El valor de la desviación estándar es: ", pd1[1].std())
+   print("-------------------------------------------\n\nAPARTADO 3")
 
 def apartado_3(con):
     cursorObj = con.cursor()
@@ -162,9 +203,9 @@ def apartado_3(con):
     rows = cursorObj.fetchall()
 
     pd1 = pd.DataFrame(rows)
-    print(pd1)
     print("El valor de la media es: ", pd1[1].mean())
     print("El valor de la desviación estándar es: ", pd1[1].std())
+    print("-------------------------------------------\n\nAPARTADO 4")
 
 def apartado_4(con):
     cursorObj = con.cursor()
@@ -172,9 +213,9 @@ def apartado_4(con):
     rows = cursorObj.fetchall()
 
     pd1 = pd.DataFrame(rows)
-    print(pd1)
     print("El valor de la media es: ", pd1[1].mean())
     print("El valor de la desviación estándar es: ", pd1[1].std())
+    print("-------------------------------------------\n\nAPARTADO 5")
 
 def apartado_5(con):
     cursorObj = con.cursor()
@@ -183,9 +224,9 @@ def apartado_5(con):
 
     pd1 = pd.DataFrame(rows)
     pd1[1] += pd1[2]  # sumo al total de fechas de cada usuario las repetidas
-    print(pd1)
     print("El valor maximo es: ", pd1[1].max())
     print("El valor mínimo es: ", pd1[1].min())
+    print("-------------------------------------------\n\nAPARTADO 6")
 
 def apartado_6(con):
     cursorObj = con.cursor()
@@ -193,9 +234,9 @@ def apartado_6(con):
     rows = cursorObj.fetchall()
 
     pd1 = pd.DataFrame(rows)
-    print(pd1)
     print("El valor maximo es: ", pd1[1].max())
     print("El valor mínimo es: ", pd1[1].min())
+    print("-------------------------------------------\n")
 
 
 con = sqlite3.connect('example.db')
@@ -204,9 +245,9 @@ con = sqlite3.connect('example.db')
 #sql_insert_json_user(con)
 #sql_insert_json_legal(con)
 apartado_1(con)
-#apartado_2(con)
-#apartado_3(con)
-#apartado_4(con)
-#apartado_5(con)
-#apartado_6(con)
+apartado_2(con)
+apartado_3(con)
+apartado_4(con)
+apartado_5(con)
+apartado_6(con)
 con.close()
--- src/Ejercicio3.py
+++ src/Ejercicio3.py
@@ -2,28 +2,31 @@ import sqlite3
 import pandas as pd
 
 def apartado_1(pd):
-   print("tienen un total de muestras de: ", pd[6].sum())
+   print("tienen un total de muestras de: ", pd["email_phishing"].sum())
 
 def apartado_2(pd):
-   print("tienen un total de valores missing de:", end=' ')
-   try:
-      print(pd[6].value_counts().loc[0])
-   except:
-      print(0)
+   # Contar valores missing
+   contador = 0
+   for fila in pd["email_phishing"]:
+      if str(fila) == 'None' or str(fila) == '-1' or str(fila) == '':
+         contador += 1
+
+   print("En la columna email_phishing hay", contador, "missing")
 
 def apartado_3(pd):
-   print("tienen un valor de mediana de: ", pd[6].median())
+   print("tienen un valor de mediana de: ", pd["email_phishing"].median())
 
 def apartado_4(pd):
-   print("tienen un valor de media de: ", pd[6].mean())
+   print("tienen un valor de media de: ", pd["email_phishing"].mean())
 
 def apartado_5(pd):
-   print("tienen un valor de varianza de: ", pd[6].var())
+   print("tienen un valor de varianza de: ", pd["email_phishing"].var())
 
 def apartado_6(pd):
-   print("tienen un valor mínimo de: ", pd[6].min(), " y máximo de: ", pd[6].max())
+   print("tienen un valor mínimo de: ", pd["email_phishing"].min(), " y máximo de: ", pd["email_phishing"].max())
 
 def resultados(pd, user, cantidad):
+   print("-------------------------------------------\n" + user, "con", cantidad, "de 200 emails\n")
    print(user, "con", cantidad, "de 200 email", end=' ')
    apartado_1(pd)
    print(user, "con", cantidad, "de 200 email", end=' ')
@@ -43,6 +46,7 @@ def main():
    cursorObj.execute('SELECT  * FROM usuarios where email_total <  200 and permisos = 0')
    rows = cursorObj.fetchall()
    pd1 = pd.DataFrame(rows)
+   pd1.rename(columns={0: "nombre", 1: "telefono", 2: "contraseña", 3: "provincia", 4: "permisos", 5: "email_total", 6: "email_phishing", 7: "email_clicados"}, inplace=True)
 
    resultados(pd1, "Usuarios", "menos")
 
@@ -50,21 +54,24 @@ def main():
    cursorObj.execute('SELECT  * FROM usuarios where email_total <  200 and permisos = 1')
    rows = cursorObj.fetchall()
    pd2 = pd.DataFrame(rows)
+   pd2.rename(columns={0: "nombre", 1: "telefono", 2: "contraseña", 3: "provincia", 4: "permisos", 5: "email_total", 6: "email_phishing", 7: "email_clicados"}, inplace=True)
 
-   resultados(pd1, "Administradores", "menos")
+   resultados(pd2, "Administradores", "menos")
 
 
    cursorObj.execute('SELECT  * FROM usuarios where email_total >=  200 and permisos = 0')
    rows = cursorObj.fetchall()
    pd3 = pd.DataFrame(rows)
+   pd3.rename(columns={0: "nombre", 1: "telefono", 2: "contraseña", 3: "provincia", 4: "permisos", 5: "email_total", 6: "email_phishing", 7: "email_clicados"}, inplace=True)
 
-   resultados(pd1, "Usuarios", "igual o más")
+   resultados(pd3, "Usuarios", "igual o más")
 
    cursorObj.execute('SELECT  * FROM usuarios where email_total >=  200 and permisos = 1')
    rows = cursorObj.fetchall()
    pd4 = pd.DataFrame(rows)
+   pd4.rename(columns={0: "nombre", 1: "telefono", 2: "contraseña", 3: "provincia", 4: "permisos", 5: "email_total", 6: "email_phishing", 7: "email_clicados"}, inplace=True)
 
-   resultados(pd1, "Administradores", "igual o más")
+   resultados(pd4, "Administradores", "igual o más")
 
 
 
