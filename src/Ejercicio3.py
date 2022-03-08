f1d20b5ea65b3a002b6b0f6135d40f46642a54ec
GeorgeChulin <j.garciaga.2019@alumnos.urjc.es>
2022-03-08 16:59:44 +0100

    Ejercicio3v2

--- src/Ejercicio3.py
+++ src/Ejercicio3.py
@@ -23,70 +23,49 @@ def apartado_5(pd):
 def apartado_6(pd):
    print("tienen un valor mínimo de: ", pd[6].min(), " y máximo de: ", pd[6].max())
 
+def resultados(pd, user, cantidad):
+   print(user, "con", cantidad, "de 200 email", end=' ')
+   apartado_1(pd)
+   print(user, "con", cantidad, "de 200 email", end=' ')
+   apartado_2(pd)
+   print(user, "con", cantidad, "de 200 email", end=' ')
+   apartado_3(pd)
+   print(user, "con", cantidad, "de 200 email", end=' ')
+   apartado_4(pd)
+   print(user, "con", cantidad, "de 200 email", end=' ')
+   apartado_5(pd)
+   print(user, "con", cantidad, "de 200 email", end=' ')
+   apartado_6(pd)
+
 def main():
    cursorObj = con.cursor()
 
    cursorObj.execute('SELECT  * FROM usuarios where email_total <  200 and permisos = 0')
    rows = cursorObj.fetchall()
    pd1 = pd.DataFrame(rows)
-   #print("Usuarios con menos de 200 email", end=' ')
-   #apartado_1(pd1)
-   #print("Usuarios con menos de 200 email", end=' ')
-   #apartado_2(pd1)
-   #print("Usuarios con menos de 200 email", end=' ')
-   #apartado_3(pd1)
-   #print("Usuarios con menos de 200 email", end=' ')
-   #apartado_4(pd1)
-   #print("Usuarios con menos de 200 email", end=' ')
-   #apartado_5(pd1)
-   #print("Usuarios con menos de 200 email", end=' ')
-   #apartado_6(pd1)
+
+   resultados(pd1, "Usuarios", "menos")
+
 
    cursorObj.execute('SELECT  * FROM usuarios where email_total <  200 and permisos = 1')
    rows = cursorObj.fetchall()
    pd2 = pd.DataFrame(rows)
-   #print("Administradores con menos de 200 email", end=' ')
-   #apartado_1(pd2)
-   #print("Administradores con menos de 200 email", end=' ')
-   #apartado_3(pd2)
-   #print("Administradores con menos de 200 email", end=' ')
-   #apartado_4(pd2)
-   #print("Administradores con menos de 200 email", end=' ')
-   #apartado_5(pd2)
-   #print("Administradores con menos de 200 email", end=' ')
-   #apartado_6(pd2)
+
+   resultados(pd1, "Administradores", "menos")
+
 
    cursorObj.execute('SELECT  * FROM usuarios where email_total >=  200 and permisos = 0')
    rows = cursorObj.fetchall()
    pd3 = pd.DataFrame(rows)
-   #print("Usuarios con igual o más de 200 email", end=' ')
-   #apartado_1(pd3)
-   #print("Usuarios con igual o más de 200 email", end=' ')
-   #apartado_2(pd3)
-   #print("Usuarios con igual o más de 200 email", end=' ')
-   #apartado_3(pd3)
-   #print("Usuarios con igual o más de 200 email", end=' ')
-   #apartado_4(pd3)
-   #print("Usuarios con igual o más de 200 email", end=' ')
-   #apartado_5(pd3)
-   #print("Usuarios con igual o más de 200 email", end=' ')
-   #apartado_6(pd3)
+
+   resultados(pd1, "Usuarios", "igual o más")
 
    cursorObj.execute('SELECT  * FROM usuarios where email_total >=  200 and permisos = 1')
    rows = cursorObj.fetchall()
    pd4 = pd.DataFrame(rows)
-   #print("Administradores con igual o más de 200 email", end=' ')
-   #apartado_1(pd4)
-   #print("Administradores con igual o más de 200 email", end=' ')
-   #apartado_2(pd4)
-   #print("Administradores con igual o más de 200 email", end=' ')
-   #apartado_3(pd4)
-   #print("Administradores con igual o más de 200 email", end=' ')
-   #apartado_4(pd4)
-   #print("Administradores con igual o más de 200 email", end=' ')
-   #apartado_5(pd4)
-   #print("Administradores con igual o más de 200 email", end=' ')
-   #apartado_6(pd4)
+
+   resultados(pd1, "Administradores", "igual o más")
+
 
 
 con = sqlite3.connect('example.db')
