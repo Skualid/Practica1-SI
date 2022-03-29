6125c4d54149155c4092c133d3b528333aba87f2
Skualid <jl.gallego.2019@alumnos.urjc.es>
2022-03-29 21:10:52 +0200

    Parámetro de users críticos
    Optimización y eliminación de código replicado para el top X de usuarios críticos

--- src/Parte 2/Ejercicio2.py
+++ src/Parte 2/Ejercicio2.py
@@ -9,36 +9,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import json
 import plotly.graph_objects as go
 
-app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-@app.route('/signin.html')
-def login():
-    name = request.args.get('user')
-    return render_template('signin.html', name=name, graphJSON=None)
-
-@app.route('/signup.html')
-def register():
-    name = request.args.get('user')
-    return render_template('signup.html', name=name, graphJSON=None)
-
-@app.route('/param', methods=['POST'])
-def param():
-    filtro = request.form['numero']
-    return plotly(filtro)
-
-
-@app.route('/plotly/<filtro>')
-def plotly(filtro):
+def usersVuln():
     con = sqlite3.connect('example.db')
     cursorObj = con.cursor()
 
@@ -70,7 +41,52 @@ def plotly(filtro):
     # Se ordena el Dataframe en función de la columna ratio
     pd1 = pd1.sort_values('ratio', ascending=False)
 
+    return pd1
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/signin.html')
+def login():
+    name = request.args.get('user')
+    return render_template('signin.html', name=name, graphJSON=None)
+
+@app.route('/signup.html')
+def register():
+    name = request.args.get('user')
+    return render_template('signup.html', name=name, graphJSON=None)
+
+@app.route('/param', methods=['POST'])
+def param():
+    filtro = request.form['numero']
+    return plotly(filtro)
+
+@app.route('/plotly.html/<filtro>')
+def plotly(filtro):
+
+    pd1 = usersVuln()
     pd1 = pd1.head(int(filtro))
+    if int(filtro) <= 0:
+        return render_template('error.html')
+    else:
+        return plotlyNoArgs(pd1)
+
+@app.route('/plotly.html')
+def plotlyNoArgs(pd2=None):
+    if pd2 is not None:
+        pd1 = pd2
+    else:
+        pd1 = usersVuln()
 
     x = pd1["nombre"]
 
@@ -104,9 +120,8 @@ def plotly(filtro):
 
     a = plotly.utils.PlotlyJSONEncoder
     graphJSON = json.dumps(fig, cls=a)
-    return render_template('hello.html', graphJSON=graphJSON)
+    return render_template('plotly.html', graphJSON=graphJSON)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
