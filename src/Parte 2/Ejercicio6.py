e9811ecdf0e9f688c69d41534374aacdecca702e
Skualid <jl.gallego.2019@alumnos.urjc.es>
2022-04-28 20:15:37 +0200

    Ejercicio 6 y práctica terminada :)

--- src/Parte 2/Ejercicio6.py
+++ src/Parte 2/Ejercicio6.py
@@ -2,13 +2,10 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 import json
-import graphviz
+import numpy as np
 
 with open('JSON Ejercicios IA-20220426/users_IA_clases.json', 'r') as f:
     data_train = json.load(f)
-
-with open('JSON Ejercicios IA-20220426/users_IA_predecir.json', 'r') as f:
-    data_test = json.load(f)
 
 matriz_train = [None] * len(data_train['usuarios'])
 for i in range(len(data_train['usuarios'])):
@@ -27,23 +24,25 @@ for usuario in range(len(data_train['usuarios'])):
 
     i += 1
 
-print(list_train)
-print(matriz_train)
-
 # Use only one feature
 data_train_Y = list_train
 data_train_X = matriz_train
-#print(data_train_Y)
 
 # Split the data into training/testing sets
-data_X_train = data_train_X[:-20]
-data_X_test = data_train_X[-20:]
+data_X_train = data_train_X[:20]
+data_X_test = data_train_X[20:]
 
 # Split the targets into training/testing sets
-data_y_train = data_train_Y[:-20]
-data_y_test = data_train_Y[-20:]
+data_y_train = data_train_Y[:20]
+data_y_test = data_train_Y[20:]
 
 def RegresionLinear():
+    res = []
+    for i in data_X_test:
+        res.append([i[1] / i[0]])
+    print(res)
+    print(data_y_test)
+
     # Create linear regression object
     regr = linear_model.LinearRegression()
 
@@ -57,30 +56,26 @@ def RegresionLinear():
 
     # The coefficients
     print("Coefficients: \n", regr.coef_)
+    # Este es el valor donde corta el eje Y (en X=0)
+    print('Independent term: \n', regr.intercept_)
     # The mean squared error
     print("Mean squared error: %.2f" % mean_squared_error(data_y_test, data_y_pred))
     # The coefficient of determination: 1 is perfect prediction
     print("Coefficient of determination: %.2f" % r2_score(data_y_test, data_y_pred))
-
+    print(len(data_X_test), len(data_y_test), len(data_y_pred))
     # Plot outputs
-    res = []
-    for i in data_X_test:
-        if i[1] == 0:
-            i[1] = 1
-        res.append([float(i[0]/i[1])])
-
-    print(res)
     plt.scatter(res, data_y_test, color="black")
-    plt.plot(res, data_y_pred, color="blue", linewidth=3)
+    plt.plot(0.02 * np.array(res) + regr.intercept_, res  , color="blue", linewidth=3)
 
-    plt.xticks(())
-    plt.yticks(())
 
+    plt.xlabel("Probabilidad de click")
+    plt.ylabel("Vulnerable no (0) si (1)")
+    plt.xticks()
+    plt.yticks()
     plt.show()
 
 
 def decisionTree():
-    from sklearn.datasets import load_iris
     from sklearn import tree
 
     regr = tree.DecisionTreeClassifier()
@@ -94,7 +89,7 @@ def forest():
     from sklearn.ensemble import RandomForestClassifier
     from sklearn import tree
 
-    regr = RandomForestClassifier(max_depth=2, random_state=0, n_estimators=10)
+    regr = RandomForestClassifier(max_depth=2, random_state=0, n_estimators=9)
     # Train the model using the training sets
     regr.fit(data_X_train, data_y_train)
 
@@ -107,6 +102,6 @@ def forest():
         tree.plot_tree(estimator, filled=True, fontsize=10, rounded = True, precision=2, proportion=False)
         plt.show()
 
-forest()
+#forest()
 #decisionTree()
-#RegresionLinear()
+RegresionLinear()
